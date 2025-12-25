3d336fed9c75adc5fc804e0136c5d0284bde3204
Sudip-Chel <sudipchel005@gmail.com>
2025-12-25 10:31:53 +0530

    vercel.json and app.jsx fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from './components/ui/button'
-import Header from './components/custom/header.jsx'
+import Header from './components/custom/Header.jsx'
 import { Outlet , Navigate } from 'react-router-dom'
 import { useUser } from '@clerk/clerk-react';
 import { Toaster } from '@/components/ui/sonner'
@@ -12,9 +12,10 @@ function App() {
   const [count, setCount] = useState(0)
   const {user , isLoaded , isSignedIn} = useUser();
 
-  if(!isSignedIn && isLoaded){
-    return <Navigate to={'/auth/signIn'}/>;
-  }
+  if (!isSignedIn && isLoaded) {
+  return <Navigate to="/sign-in" replace />;
+}
+
   return (
     <>
       <Header/>
